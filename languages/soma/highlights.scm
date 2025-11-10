0b80e429a53e01960e3b6545c4c5081d7a857260
Gabriel <gaabrielms@proton.me>
2025-11-10 09:51:28 -0500

    feat: keyword, primites, types & fn highlighting

--- languages/soma/highlights.scm
+++ languages/soma/highlights.scm
@@ -14,3 +14,14 @@
   "then"
   "else"
 ] @keyword
+
+(comment) @comment
+
+(string_literal) @string
+(integer_literal) @number
+(bool_literal) @boolean
+(list_literal) @list
+(type_name) @type
+
+(function_declaration
+  name: (identifier) @function)
