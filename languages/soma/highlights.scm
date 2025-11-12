115d31d02a1f932345b69681932d1d985e3f31c7
Gabriel <gaabrielms@proton.me>
2025-11-12 13:17:17 -0500

    feat: better syntax highlighting

--- languages/soma/highlights.scm
+++ languages/soma/highlights.scm
@@ -1,18 +1,18 @@
 [
-  "use"
-  "intrinsic"
-  "data"
-  "trait"
-  "instance"
-  "where"
-  "def"
-  "let"
-  "in"
-  "compose"
-  "bind"
-  "if"
-  "then"
-  "else"
+  (if)
+  (then)
+  (else)
+  (let)
+  (in)
+  (compose)
+  (bind)
+  (where)
+  (def)
+  (intrinsic)
+  (data)
+  (trait)
+  (instance)
+  (import)
 ] @keyword
 
 (comment) @comment
@@ -21,7 +21,67 @@
 (integer_literal) @number
 (bool_literal) @boolean
 (list_literal) @list
-(type_name) @type
 
 (function_declaration
-  name: (identifier) @function)
+  name: (_) @function
+)
+
+(function_signature
+  name: (_) @function
+)
+
+(data_type_declaration
+  name: (type_name) @type
+)
+
+(data_type_declaration
+  type_parameters: (identifier) @type
+)
+
+(intrinsic_data_type
+  name: (type_name) @type
+)
+
+(trait_constraint
+  parameter: (identifier) @type
+  trait: (type_name) @type
+)
+
+(constructor_pattern
+  constructor: (type_name) @constructor
+)
+
+(import_path
+  (identifier) @module)
+
+(type) @type
+
+[
+  (arrow)
+  (equal)
+  (colon)
+  (colon_colon)
+  (bar)
+  (double_arrow)
+  (operator)
+  (reverse_arrow)
+  "<-"
+] @operator
+
+(constructor_name) @constructor
+
+(app_expression
+  function: (identifier) @function)
+
+[
+    (lbraces)
+    (rbraces)
+    "("
+    ")"
+] @punctuation.bracket
+
+[
+    "."
+    "/"
+    ","
+] @punctuation.delimiter
